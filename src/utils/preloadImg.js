244275e81f70010ca4eed89c22143e68fbe66d47
Adam Bergman <adam@fransvilhelm.com>
2017-09-14 00:01:16 +0200

    :art: Update proper error returned from preloadImg

--- src/utils/preloadImg.js
+++ src/utils/preloadImg.js
@@ -1,11 +1,20 @@
 async function preloadImg(src) {
   try {
     const res = await fetch(src);
+
+    if (res.status < 200 || res.status > 299) {
+      const error = new Error(res.statusText);
+      error.src = src;
+      error.code = res.status;
+      throw error;
+    }
+
     const blob = await res.blob();
     const blobUrl = URL.createObjectURL(blob);
+
     return blobUrl;
-  } catch (e) {
-    throw src;
+  } catch (error) {
+    throw error;
   }
 }
 
