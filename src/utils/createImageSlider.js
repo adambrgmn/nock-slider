ae2881a7e08a7382eb1ebf2fefacefaaa2a244b8
Adam Bergman <adam@fransvilhelm.com>
2017-09-13 14:31:55 +0200

    :zap: Preload blob instead of relying on img onload event

--- src/utils/createImageSlider.js
+++ src/utils/createImageSlider.js
@@ -1,32 +1,34 @@
 import filter from 'ramda/src/filter';
 import rafScheduler from 'raf-schd';
 import delay from './delay';
-
-const BASE_CLASS = 'nock-img';
+import { imgClass } from '../config';
 
 function createImageSlider(parent, transitionDelay = 0) {
-  return async img => {
+  return async blobUrl => {
     const existingImg = parent.firstChild;
     const children = Array.from(parent.children);
+    const nextImg = document.createElement('img');
+    nextImg.classList.add(imgClass);
+    nextImg.setAttribute('src', blobUrl);
 
-    if (transitionDelay === 0 && existingImg) existingImg.replaceWith(img);
+    if (transitionDelay === 0 && existingImg) existingImg.replaceWith(nextImg);
 
     const enterImages = rafScheduler(() => {
       children.forEach(child => {
-        child.classList.remove(`${BASE_CLASS}-enter`);
-        child.classList.add(`${BASE_CLASS}-leave`);
+        child.classList.remove(`${imgClass}-enter`);
+        child.classList.add(`${imgClass}-leave`);
       });
 
-      img.classList.add(`${BASE_CLASS}-enter`);
-      parent.appendChild(img);
+      nextImg.classList.add(`${imgClass}-enter`);
+      parent.appendChild(nextImg);
     });
 
     const leaveImages = rafScheduler(() => {
       children.forEach(child => {
-        child.classList.remove(`${BASE_CLASS}-leave`);
+        child.classList.remove(`${imgClass}-leave`);
         child.remove();
       });
-      img.classList.remove(`${BASE_CLASS}-enter`);
+      nextImg.classList.remove(`${imgClass}-enter`);
     });
 
     enterImages();
--- src/utils/preloadImg.js
+++ src/utils/preloadImg.js
@@ -3,12 +3,7 @@ async function preloadImg(src) {
     const res = await fetch(src);
     const blob = await res.blob();
     const blobUrl = URL.createObjectURL(blob);
-
-    const img = document.createElement('img');
-    img.setAttribute('src', blobUrl);
-    img.classList.add('nock-img');
-
-    return img;
+    return blobUrl;
   } catch (e) {
     throw src;
   }
